40b5ff2eb24103b59906cdf95900cc455e7a1832
hemant1101 <hemantmandal1101@gmail.com>
2023-03-23 16:27:47 +0530

    user register api done

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require('mongoose')
 
 const dbConnect = () => {
     try {
-        const conn = mongoose.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@${process.env.CLUSTER}.o1vbmd1.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`)
+        const conn = mongoose.connect(process.env.MONGODB_URL)
 
         console.log('Database Connected Successfully.')
     }

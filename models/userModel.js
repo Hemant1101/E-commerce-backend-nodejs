49da3789bf0e19bbee99075848abcdf5e7e89516
hemant1101 <hemantmandal1101@gmail.com>
2023-03-29 14:49:22 +0530

    login api created

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 const { default: mongoose } = require("mongoose");
-
+const bcrypt = require('bcrypt')
 var userSchema = new mongoose.Schema({
     firstname: {
         type: String,
@@ -22,6 +22,20 @@ var userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    role: {
+        type: String,
+        default: 'user'
+    }
 })
+
+
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSaltSync(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
 
 module.exports = mongoose.model("User", userSchema)
